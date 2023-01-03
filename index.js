7414def423bb79c3d2d0db67161b5106126e4fd3
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-01-03 14:48:11 -0700

    commit tree 60221e3d96c0f456db407e811754d4b4ebfcd7bb

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(usersName, usersEmail){
+    const obj = {
+        name: usersName,
+        email: usersEmail
+    }
+   return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",   
+        },
+        body: JSON.stringify(obj),
+    })
+    .then((response) => response.json())
+    .then((obj) => document.body.innerHTML = obj.id)
+    .catch(function (error) {
+        // alert("Bad things! Ragnarők!");
+        document.body.innerHTML = error.message;
+})
+}
+
